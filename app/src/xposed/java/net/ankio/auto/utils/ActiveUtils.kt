2300e528f1f947efb2fea89c8420146cf1d27e59
Ankio <ankio@ankio.net>
2024-03-13 15:46:26 +0800

    :zap: 精简激活工具

--- app/src/xposed/java/net/ankio/auto/utils/ActiveUtils.kt
+++ app/src/xposed/java/net/ankio/auto/utils/ActiveUtils.kt
@@ -18,99 +18,47 @@ package net.ankio.auto.utils
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import net.ankio.auto.App
+import android.content.SharedPreferences
+import com.crossbowffs.remotepreferences.RemotePreferences
 import net.ankio.auto.BuildConfig
 import net.ankio.auto.R
-import net.ankio.auto.database.table.AccountMap
-import net.ankio.auto.database.table.AppData
-import net.ankio.auto.hooks.android.AccountingService
-import net.ankio.auto.ui.activity.RestartActivity
+import net.ankio.auto.database.table.AssetsMap
 
 
 object ActiveUtils {
     fun getActiveAndSupportFramework(context: Context): Boolean {
-        if(AccountingService.get()===null){
-            val intent = Intent(context, RestartActivity::class.java)
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context.startActivity(intent)
-        }
         return false
     }
-    fun errorMsg(context: Context):String{
-        return context.packageManager.getPackageInfo(context.packageName, 0).versionName
-    }
 
-    fun startApp(mContext:Context){
-        val intent: Intent? = mContext.packageManager.getLaunchIntentForPackage("net.ankio.auto.xposed")
+    fun startApp(mContext: Context) {
+        val intent: Intent? =
+            mContext.packageManager.getLaunchIntentForPackage(BuildConfig.APPLICATION_ID)
         if (intent != null) {
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             mContext.startActivity(intent)
         }
     }
-    fun onStartApp(activity: Activity){
 
-    }
-    //仅Hook环境有效，所以此处不实现，交给hook
-    fun get(key:String,app:String=BuildConfig.APPLICATION_ID): String {
-        val sharedPreferences = App.context.getSharedPreferences("AutoAccounting.$app", Context.MODE_PRIVATE)
-        return sharedPreferences.getString(key,"")?:""
-    }
-    //非hook环境
-    fun put(key:String,value:String){
-        val sharedPreferences = App.context.getSharedPreferences("AutoAccounting.${BuildConfig.APPLICATION_ID}", Context.MODE_PRIVATE)
-         sharedPreferences.edit().putString(key,value).apply()
-    }
-    //仅hook环境有效
-    fun getAccountMap():List<AccountMap>{
-      return arrayListOf()
-    }
 
-    /**
-     * 非Hook环境
-     * 获取日志，Xposed模式下，日志从目标app读取
-     */
-    fun getLogList(context: Context): String {
-        var log = "";
-        for (app in context.resources.getStringArray(R.array.xposed_scope)){
-            log += get("log",app)
+    var APPLICATION_ID = BuildConfig.APPLICATION_ID
+
+    private  var TOKEN = ""
+
+    fun getToken(context: Context): String {
+        if(TOKEN.isNotEmpty())return TOKEN
+        val auth = "net.ankio.preferences"
+        val file = "main_prefs"
+        if (APPLICATION_ID == BuildConfig.APPLICATION_ID) {
+            val token = AutoAccountingServiceUtils.get("token")
+            TOKEN = token
+            val prefs = context.getSharedPreferences(file, Context.MODE_PRIVATE)
+            prefs.edit().putString("token", token).apply()
+            return token
+        } else {
+
+            val prefs: SharedPreferences = RemotePreferences(context, auth, file)
+            TOKEN = prefs.getString("token", "") ?: ""
+            return TOKEN
         }
-
-        return sortLogs(log)
     }
-    /**
-     * 非Hook环境
-     * 获取APP数据
-     */
-    fun getDataList(currentPage:Int,itemsPerPage:Int,context: Context,callback: (list: List<AppData>) -> Unit) {
-        val list = arrayListOf<AppData>();
-        for (app in context.resources.getStringArray(R.array.xposed_scope)){
-            runCatching {
-                val data = get("billData",app)
-                for (line in data.lines()){
-                    if(line.isNotEmpty()){
-                        runCatching {
-                            list.add(AppData.fromJSON(line.replace("___r_n","\n")))
-                        }.onFailure {
-                            Log.e("数据异常",line)
-                            Log.e("异常",it.message.toString())
-                        }
-                    }
-                }
-            }
-        }
-        callback(list.sortedBy { it.time })
-    }
-    private fun sortLogs(logString: String): String {
-        val regex = Regex("\\[(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\\]") // 正则表达式匹配日期和时间
-        return logString.split("\n") // 将字符串分割成日志条目列表
-            .map { it.trim().replace("___r_n","\n") } // 移除每行的首尾空白
-            .filter { it.isNotBlank() } // 过滤掉空白行
-            .sortedBy { regex.find(it)?.groups?.get(1)?.value } // 排序
-            .joinToString("\n") // 用换行符重新连接
-    }
-
-
-
-
 }
