b262180c3196f3e681841b1acce705718f336a42
Ankio <ankio@ankio.net>
2024-03-27 13:39:30 +0800

    :sparkles: 错误重定向到ErrorActivity

---
+++ app/src/main/java/net/ankio/auto/ui/activity/ErrorActivity.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2024 ankio(ankio@ankio.net)
+ * Licensed under the Apache License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-3.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package net.ankio.auto.ui.activity
+
+import android.os.Bundle
+import com.zackratos.ultimatebarx.ultimatebarx.addNavigationBarBottomPadding
+import com.zackratos.ultimatebarx.ultimatebarx.addStatusBarTopPadding
+import net.ankio.auto.databinding.ActivityErrorBinding
+import net.ankio.auto.utils.AppUtils
+
+class ErrorActivity:  BaseActivity() {
+    private lateinit var binding: ActivityErrorBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityErrorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = intent
+
+        if (intent == null) {
+            AppUtils.restart()
+            return
+        }
+
+        val msg = intent.getStringExtra("msg")
+        binding.errorMsg.text = msg
+        binding.errorRestart.setOnClickListener {
+            AppUtils.restart()
+        }
+        onViewCreated()
+        binding.main.addStatusBarTopPadding()
+        binding.main.addNavigationBarBottomPadding()
+    }
+}
--- app/src/main/java/net/ankio/auto/utils/ExceptionHandler.kt
+++ app/src/main/java/net/ankio/auto/utils/ExceptionHandler.kt
@@ -18,56 +18,63 @@ package net.ankio.auto.utils
 import android.content.Context
 import android.content.Intent
 import com.bugsnag.android.Bugsnag
+import com.bugsnag.android.Event
 import net.ankio.auto.BuildConfig
+import net.ankio.auto.events.AutoServiceErrorEvent
 import net.ankio.auto.exceptions.AutoServiceException
+import net.ankio.auto.ui.activity.ErrorActivity
+import net.ankio.auto.utils.event.EventBus
 
 
-class ExceptionHandler : Thread.UncaughtExceptionHandler {
-    private var mDefaultHandler: Thread.UncaughtExceptionHandler? = null
+class ExceptionHandler {
 
     private var context: Context? = null
     /**
      * 初始化默认异常捕获
      */
     fun init(context: Context?) {
-        // 获取默认异常处理器
-        mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler()
-        // 将当前类设为默认异常处理器
-        Thread.setDefaultUncaughtExceptionHandler(this)
         if(SpUtils.getBoolean("sendToAppCenter",true)){
             Bugsnag.start(context!!)
+            Bugsnag.addOnError { event ->
+                handleException(event) // 发送此异常
+            }
         }
         this.context = context
     }
 
-    override fun uncaughtException(t: Thread, e: Throwable) {
-        if (!handleException(getRootCause(e))) {
-            // 如果不处理,则调用系统默认处理异常,弹出系统强制关闭的对话框
-            if (mDefaultHandler != null) {
-                mDefaultHandler!!.uncaughtException(t, e)
-            }
-        }
-    }
 
-    private fun handleException(e: Throwable?): Boolean {
-        if (e == null) {
+    private fun handleException(events:Event): Boolean {
+        val error = events.originalError
+        if(error == null){
+            val msg = events.errors[0].errorMessage?:""
+            Logger.e("发生内容为NULL的异常 => $msg")
             return false
         }
-        Logger.e("未处理异常",e)
-        if(e is AutoServiceException){
-        /*    context?.let {
-                val intent = Intent(it, ServiceActivity::class.java).apply {
-                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                }
-                it.startActivity(intent)
-            }*/
-        }else{
-            if(SpUtils.getBoolean("sendToAppCenter",true) && !BuildConfig.DEBUG){
-                Bugsnag.notify(e)
-            }
-            //TODO 跳转错误页面，在错误页面重新启动
+        val root = getRootCause(error)
+        if(root is AutoServiceException){
+          EventBus.post(AutoServiceErrorEvent(root))
+         return false
         }
-        return true
+
+        Logger.e("发生未处理的异常", root,true)
+
+        //将异常拼成字符串
+        val sb = StringBuilder()
+        sb.append("版本: ${BuildConfig.VERSION_NAME}\n")
+        sb.append("版本号: ${BuildConfig.VERSION_CODE}\n")
+        sb.append("异常信息: ${root.message}\n")
+        sb.append("异常堆栈: \n")
+        root.stackTrace.forEach {
+            sb.append(it.toString())
+            sb.append("\n")
+        }
+        //打开ErrorActivity
+        val intent = Intent(context, ErrorActivity::class.java)
+        intent.putExtra("msg", sb.toString())
+        context?.startActivity(intent)
+
+        //调试模式不上传错误数据
+        return !AppUtils.getDebug()
     }
 
     private fun getRootCause(e: Throwable): Throwable {
