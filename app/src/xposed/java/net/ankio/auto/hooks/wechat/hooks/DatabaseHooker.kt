be1a38a84ca798565c57236ce1aab496c23f9618
Ankio <ankio@ankio.net>
2024-05-02 22:36:21 +0800

    :bug: 修复微信支付缺少金额信息的问题 #79 #74

--- app/src/xposed/java/net/ankio/auto/hooks/wechat/hooks/DatabaseHooker.kt
+++ app/src/xposed/java/net/ankio/auto/hooks/wechat/hooks/DatabaseHooker.kt
@@ -22,120 +22,122 @@ import com.google.gson.JsonObject
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedHelpers
 import fr.arnaudguyon.xmltojsonlib.XmlToJson
-import kotlinx.coroutines.runBlocking
 import net.ankio.auto.api.Hooker
 import net.ankio.auto.api.PartHooker
 import net.ankio.auto.constant.DataType
 
-
-
-class DatabaseHooker (hooker: Hooker) : PartHooker(hooker){
-
-    override fun onInit(classLoader: ClassLoader, context: Context) {
+class DatabaseHooker(hooker: Hooker) : PartHooker(hooker) {
+    override fun onInit(
+        classLoader: ClassLoader,
+        context: Context,
+    ) {
         val mAppClassLoader: ClassLoader = classLoader
         val mContext: Context = context
 
-        //分析版本 8.0.43
+        // 分析版本 8.0.43
 
         val database = XposedHelpers.findClass("com.tencent.wcdb.database.SQLiteDatabase", mAppClassLoader)
-        XposedHelpers.findAndHookMethod(database, "insert",
+        XposedHelpers.findAndHookMethod(
+            database,
+            "insert",
             String::class.java,
             String::class.java,
-            ContentValues::class.java, object : XC_MethodHook() {
+            ContentValues::class.java,
+            object : XC_MethodHook() {
                 @Throws(Throwable::class)
                 override fun afterHookedMethod(param: MethodHookParam) {
                     val contentValues = param.args[2] as ContentValues
                     val tableName = param.args[0] as String
-                    val arg = param.args[1] as String
+                    val arg = if (param.args[1] !== null)param.args[1] as String else ""
 
                     logD("微信数据：${Gson().toJson(contentValues)} table:$tableName arg:$arg")
 
                     val type = contentValues.getAsInteger("type") ?: return
-                    //补充数据
-                    contentValues.put("tableName",tableName)
-                    contentValues.put("arg",arg)
+                    // 补充数据
+                    contentValues.put("tableName", tableName)
+                    contentValues.put("arg", arg)
 
-                    if(tableName == "message"){
-                        if(type == 1){
-                            //这是聊天消息，content就是对话内容
-                        }else if (type == 318767153){
-                            //这是消息盒子
+                    if (tableName == "message") {
+                        if (type == 1) {
+                            // 这是聊天消息，content就是对话内容
+                        } else if (type == 318767153) {
+                            // 这是消息盒子
                             val content = contentValues.get("content").toString()
-                            if(!content.contains("CDATA[微信支付"))return //只对微信支付特殊处理
-                            val json = Gson().fromJson(xmlToJson(content),JsonObject::class.java)
-                            val tpl = Gson().fromJson("""
-                                {
-                                  "description": "",
-                                        "source": "微信支付",
-                                        "type": 5,
-                                        "appId": "",
-                                        "msgId": 99064,
-                                        "title": ""
-                                }
-                            """.trimIndent(),JsonObject::class.java)
+                            if (!content.contains("CDATA[微信支付"))return // 只对微信支付特殊处理
+                            val json = Gson().fromJson(xmlToJson(content), JsonObject::class.java)
+                            val tpl =
+                                Gson().fromJson(
+                                    """
+                                    {
+                                      "description": "",
+                                            "source": "微信支付",
+                                            "type": 5,
+                                            "appId": "",
+                                            "msgId": 99064,
+                                            "title": ""
+                                    }
+                                    """.trimIndent(),
+                                    JsonObject::class.java,
+                                )
 
-                         //   logD("微信支付数据JSON：$json")
+                            //   logD("微信支付数据JSON：$json")
 
                             val msg = json.get("msg").asJsonObject.get("appmsg").asJsonObject
-                            tpl.addProperty("description",msg.get("des").asString)
-                            tpl.addProperty("title",msg.get("title").asString)
-                            tpl.addProperty("display_name",msg.get("mmreader").asJsonObject.get("template_header").asJsonObject.get("display_name").asString)
-
-
+                            tpl.addProperty("description", msg.get("des").asString)
+                            tpl.addProperty("title", msg.get("title").asString)
+                            tpl.addProperty(
+                                "display_name",
+                                msg.get("mmreader")
+                                    .asJsonObject.get("template_header")
+                                    .asJsonObject.get("display_name").asString,
+                            )
+                            tpl.addProperty("cachedPayTools", hooker.hookUtils.readData("cachedPayTools"))
+                            tpl.addProperty("cachedPayMoney", hooker.hookUtils.readData("cachedPayMoney"))
+                            tpl.addProperty("cachedPayShop", hooker.hookUtils.readData("cachedPayShop"))
                             val result = JsonObject()
-                            result.add("mMap",tpl)
+                            result.add("mMap", tpl)
 
                             logD("微信支付数据：$result")
 
-                            analyzeData(DataType.App.ordinal,result.toString())
-
+                            analyzeData(DataType.App.ordinal, result.toString())
                         }
-                    }else if (tableName == "AppMessage"){
-
-                        if(type == 5){
-                            if(contentValues.get("source").equals("微信支付")){
-                                //微信支付
+                    } else if (tableName == "AppMessage") {
+                        if (type == 5) {
+                            if (contentValues.get("source").equals("微信支付")) {
+                                // 微信支付
                                 return
                             }
-                            //这个应该是公众号推送
-                            analyzeData(DataType.App.ordinal,Gson().toJson(contentValues))
+                            // 这个应该是公众号推送
+                            analyzeData(DataType.App.ordinal, Gson().toJson(contentValues))
                             return
-                        }else if (type == 2000){
-                            //补充用户数据
-                            contentValues.put("hookUser",hooker.hookUtils.readData("hookerUser"))
-                            //这个应该是微信转账给别人
+                        } else if (type == 2000) {
+                            // 补充用户数据
+                            contentValues.put("hookUser", hooker.hookUtils.readData("hookerUser"))
+                            // 这个应该是微信转账给别人
                             val xml = contentValues.get("xml")
 
-                            if(xml!== null){
-                                contentValues.put("xml",xmlToJson(xml as String))
-                                contentValues.put("cachedPayTools",hooker.hookUtils.readData("cachedPayTools"))
-                                contentValues.put("cachedPayMoney",hooker.hookUtils.readData("cachedPayMoney"))
-                                contentValues.put("cachedPayShop",hooker.hookUtils.readData("cachedPayShop"))
-
+                            if (xml !== null) {
+                                contentValues.put("xml", xmlToJson(xml as String))
+                                contentValues.put("cachedPayTools", hooker.hookUtils.readData("cachedPayTools"))
+                                contentValues.put("cachedPayMoney", hooker.hookUtils.readData("cachedPayMoney"))
+                                contentValues.put("cachedPayShop", hooker.hookUtils.readData("cachedPayShop"))
                             }
-
                         }
-                    }else if(tableName == "bizchatmessage"){
-                        //好像是小程序消息
-                        if(type == 49){ //没啥有用的数据好像
-                          /*  val xml = contentValues.get("xml")
-                            if(xml!== null){
-                                contentValues.put("xml",xmlToJson(xml as String))
-
-                            }*/
+                    } else if (tableName == "bizchatmessage") {
+                        // 好像是小程序消息
+                        if (type == 49) { // 没啥有用的数据好像
                         }
                     }
-
                 }
-            })
+            },
+        )
     }
 
     fun xmlToJson(xml: String): String {
         val xmlToJson: XmlToJson = XmlToJson.Builder(xml).build()
         return xmlToJson.toString()
     }
+
     override val hookName: String
         get() = "微信数据库"
-
-
 }
--- app/src/xposed/java/net/ankio/auto/hooks/wechat/hooks/PayToolsHooker.kt
+++ app/src/xposed/java/net/ankio/auto/hooks/wechat/hooks/PayToolsHooker.kt
@@ -17,43 +17,43 @@ package net.ankio.auto.hooks.wechat.hooks
 
 import android.content.Context
 import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 import net.ankio.auto.api.Hooker
 import net.ankio.auto.api.PartHooker
-import java.lang.ref.WeakReference
 
-
-class PayToolsHooker(hooker: Hooker) : PartHooker(hooker){
+class PayToolsHooker(hooker: Hooker) : PartHooker(hooker) {
     override val hookName: String
         get() = "支付方式Hook"
 
-    override fun onInit(classLoader: ClassLoader, context: Context) {
-      XposedHelpers.findAndHookMethod(
-          "com.tencent.kinda.framework.widget.base.MMKRichText",
-          classLoader,
-          "appendText",
+    override fun onInit(
+        classLoader: ClassLoader,
+        context: Context,
+    ) {
+        XposedHelpers.findAndHookMethod(
+            "com.tencent.kinda.framework.widget.base.MMKRichText",
+            classLoader,
+            "appendText",
             String::class.java,
             object : XC_MethodHook() {
                 override fun afterHookedMethod(param: MethodHookParam) {
                     val text = param.args[0] as String
                     logD("Text: $text")
-                    when{
-                        text.contains("卡(") || text.contains("零钱")  -> {
+                    when {
+                        text.contains("卡(") || text.contains("零钱") -> {
                             logD("支付方式Hook: $text")
-                            hooker.hookUtils.writeData("cachedPayTools",text)
+                            hooker.hookUtils.writeData("cachedPayTools", text)
                         }
-                        text.contains("￥") || text.contains("$")  -> {
+                        text.contains("￥") || text.contains("$") -> {
                             logD("支付金额Hook: $text")
-                            hooker.hookUtils.writeData("cachedPayMoney",text)
+                            hooker.hookUtils.writeData("cachedPayMoney", text)
                         }
-                        text.contains("转账") || text.contains("红包")  -> {
+                        text.contains("转账") || text.contains("红包") || text.contains("付款给") -> {
                             logD("支付对象hook: $text")
-                            hooker.hookUtils.writeData("cachedPayShop",text)
+                            hooker.hookUtils.writeData("cachedPayShop", text)
                         }
                     }
                 }
-            }
-      )
+            },
+        )
     }
 }
