23e06268a1406cf821be73d4f4d3bf40c9af4775
Ankio <ankio@ankio.net>
2024-06-15 08:45:17 +0800

    :art: 默认所有功能全部开启

--- common/src/main/java/net/ankio/common/config/AccountingConfig.kt
+++ common/src/main/java/net/ankio/common/config/AccountingConfig.kt
@@ -16,12 +16,12 @@
 package net.ankio.common.config
 
 data class AccountingConfig(
-    var assetManagement: Boolean = false,//是否开启资产管理
-    var multiCurrency: Boolean = false,//是否开启多币种
-    var reimbursement: Boolean = false,//是否开启报销
-    var lending: Boolean = false,//是否开启债务功能
-    var multiBooks: Boolean = false,//是否开启多账本
-    var fee: Boolean = false,//是否开启手续费
+    var assetManagement: Boolean = true,//是否开启资产管理
+    var multiCurrency: Boolean = true,//是否开启多币种
+    var reimbursement: Boolean = true,//是否开启报销
+    var lending: Boolean = true,//是否开启债务功能
+    var multiBooks: Boolean = true,//是否开启多账本
+    var fee: Boolean = true,//是否开启手续费
 ){
     override fun toString(): String {
         return "AccountingConfig(assetManagement=$assetManagement, multiCurrency=$multiCurrency, reimbursement=$reimbursement, lending=$lending, multiBooks=$multiBooks, fee=$fee)"
