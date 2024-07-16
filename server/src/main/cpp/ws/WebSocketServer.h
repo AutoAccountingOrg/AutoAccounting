dcff8a7b9d4070505a7adea08813c0c2c427cee5
Ankio <ankio@ankio.net>
2024-07-16 19:52:23 +0800

    :fire: Remove unused code in WebSocketServer.h
    Removed unused log function, js execution logic, token and version variables, and result map.

--- server/src/main/cpp/ws/WebSocketServer.h
+++ server/src/main/cpp/ws/WebSocketServer.h
@@ -5,10 +5,7 @@
 #ifndef AUTOACCOUNTING_WEBSOCKETSERVER_H
 #define AUTOACCOUNTING_WEBSOCKETSERVER_H
 
-#define LOG_LEVEL_DEBUG 0
-#define LOG_LEVEL_INFO 1
-#define LOG_LEVEL_WARN 2
-#define LOG_LEVEL_ERROR 3
+
 
 
 #include "../wsServer/include/ws.h"
@@ -19,7 +16,6 @@
 class WebSocketServer {
 public:
     explicit WebSocketServer(int port);
-    static void log(const std::string &msg,int level);
 
 private:
     static void startServer(int port);
@@ -28,14 +24,8 @@ private:
     static void onOpen(ws_cli_conn_t *client);
     static void onClose(ws_cli_conn_t *client);
     static void onMessage(ws_cli_conn_t *client, const unsigned char *msg, uint64_t size, int type);
-    static std::string runJs(const std::string &js);
-    static std::string token;
-    static std::map<std::thread::id, std::string> resultMap;
-    static std::mutex resultMapMutex;
-    static void print(qjs::rest<std::string> args);
     struct ws_server wsServer{};
-    static std::string version;
-    static std::string getVersion();
+
 
 };
 
