16f1e9aba21614e67b46039bda2d858a6587a755
ankio <ankio@ankio.net>
2024-08-27 19:20:52 +0800

    :fire: 合并部分hook逻辑

--- app/src/xposed/java/net/ankio/auto/Apps.kt
+++ app/src/xposed/java/net/ankio/auto/Apps.kt
@@ -19,14 +19,12 @@ import net.ankio.auto.core.api.HookerManifest
 import net.ankio.auto.hooks.alipay.AliPayHooker
 import net.ankio.auto.hooks.android.AndroidHooker
 import net.ankio.auto.hooks.auto.AutoHooker
-import net.ankio.auto.hooks.keychain.KeyChainHooker
 import net.ankio.auto.hooks.wechat.WechatHooker
 
 object Apps {
     fun get() : MutableList<HookerManifest>{
         return mutableListOf(
             AndroidHooker(), // Android
-            KeyChainHooker(), // SystemUI
             AutoHooker(), // Auto
             ////////////////////////////
             WechatHooker(), // Wechat
