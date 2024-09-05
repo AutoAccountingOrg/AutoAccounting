6ee78979a86b30ba82b462f57495eda13948b7d2
ankio <ankio@ankio.net>
2024-09-05 22:00:15 +0800

    :art: (constant): 添加支出和收入账单类型
    - 完善支出和收入账单类型的定义，添加新的类型：
        - 支出（记作报销）
        - 支出（借出）
        - 支出（还款销账）
        - 收入（借入）
        - 收入（还款销账）
        - 收入（报销）

--- server/src/main/java/org/ezbook/server/constant/BillType.kt
+++ server/src/main/java/org/ezbook/server/constant/BillType.kt
@@ -17,15 +17,18 @@ package org.ezbook.server.constant
 
 enum class BillType{
     Expend,//支出
+
     ExpendReimbursement,//支出（记作报销）
     ExpendLending,//支出（借出）
     ExpendRepayment,//支出（还款销账）
 
 
     Income,//收入
+
     IncomeLending,//收入（借入）
     IncomeRepayment,//收入（还款销账）
     IncomeReimbursement,//收入（报销）
+
     Transfer;//转账
 
 }
