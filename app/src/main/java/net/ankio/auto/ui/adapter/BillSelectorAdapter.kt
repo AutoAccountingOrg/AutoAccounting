fb0397fd6421d23b3f054b4791146075dc796d47
ankio <ankio@ankio.net>
2024-09-09 19:24:04 +0800

    :recycle: (ui): 优化账单选择器适配器
    - 优化账单选择器适配器的 UI 和逻辑，使其更加清晰易懂。
    - 简化代码结构，并提高代码可读性。
    - 修复了一些小问题，并优化了用户体验。

--- app/src/main/java/net/ankio/auto/ui/adapter/BillSelectorAdapter.kt
+++ app/src/main/java/net/ankio/auto/ui/adapter/BillSelectorAdapter.kt
@@ -1,84 +1,60 @@
-/*
- * Copyright (C) 2023 ankio(ankio@ankio.net)
- * Licensed under the Apache License, Version 3.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-3.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *   limitations under the License.
- *//*
-
-
 package net.ankio.auto.ui.adapter
 
-import android.graphics.Color
-import android.view.View
-import androidx.core.content.ContextCompat
-import net.ankio.auto.app.BillUtils
-import net.ankio.auto.databinding.AdapterBillBookBinding
-import net.ankio.auto.utils.AppUtils
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import net.ankio.auto.databinding.AdapterBookBillBinding
+import net.ankio.auto.ui.api.BaseAdapter
+import net.ankio.auto.ui.api.BaseViewHolder
+import net.ankio.auto.ui.scope.autoDisposeScope
+import net.ankio.auto.ui.utils.ResourceUtils
+import net.ankio.auto.utils.BillTool
 import net.ankio.auto.utils.DateUtils
-import net.ankio.auto.utils.server.model.BookBill
-import net.ankio.common.constant.BillType
+import org.ezbook.server.constant.BillType
+import org.ezbook.server.db.model.BookBillModel
 
 class BillSelectorAdapter(
-    override val dataItems: List<BookBill>,
-    private val selectedItems: ArrayList<String>,
-) : BaseAdapter(dataItems, AdapterBillBookBinding::class.java) {
-    override fun onInitView(holder: BaseViewHolder) {
-        val binding = holder.binding as AdapterBillBookBinding
+    list: MutableList<BookBillModel>,
+    private val selectApp: MutableList<String>
+): BaseAdapter<AdapterBookBillBinding, BookBillModel>(
+    AdapterBookBillBinding::class.java, list) {
+    override fun onInitViewHolder(holder: BaseViewHolder<AdapterBookBillBinding, BookBillModel>) {
+        val binding = holder.binding
         binding.root.setOnClickListener {
-            val item = holder.item as BookBill
-            if (selectedItems.contains(item.billId)) {
-                selectedItems.remove(item.billId)
-                // 清除背景
-                binding.root.setBackgroundColor(Color.TRANSPARENT)
-            } else {
-                selectedItems.add(item.billId)
-                binding.root.setBackgroundColor(AppUtils.getThemeAttrColor(com.google.android.material.R.attr.colorPrimaryContainer))
+            val item = holder.item ?: return@setOnClickListener
+
+          if (selectApp.contains(item.remoteId)){
+              selectApp.remove(item.remoteId)
+              binding.checkbox.isChecked = false
+          }else{
+                selectApp.add(item.remoteId)
+              binding.checkbox.isChecked = true
+            }
+            Log.d("BillSelectorDialog", "selectedBills: $selectApp")
+        }
+    }
+
+    override fun onBindViewHolder(
+        holder: BaseViewHolder<AdapterBookBillBinding, BookBillModel>,
+        data: BookBillModel,
+        position: Int
+    ) {
+        val binding = holder.binding
+        binding.category.setText(data.category)
+        BillTool.setTextViewPrice(data.money, BillType.Expend, binding.money)
+        holder.binding.root.autoDisposeScope.launch {
+            ResourceUtils.getCategoryDrawableByName(data.category,holder.context).let {
+                withContext(Dispatchers.Main){
+                    binding.category.setIcon(it)
+                }
             }
         }
+        binding.checkbox.isChecked = selectApp.contains(data.remoteId)
+
+        binding.date.text = DateUtils.stampToDate(data.time, "yyyy-MM-dd HH:mm")
+
+
     }
 
-    override fun onBindView(
-        holder: BaseViewHolder,
-        item: Any,
-    ) {
-        val binding = holder.binding as AdapterBillBookBinding
-        val it = item as BookBill
-        val context = holder.context
-
-
-
-        if (selectedItems.contains(it.billId)) {
-            binding.root.setBackgroundColor(AppUtils.getThemeAttrColor(com.google.android.material.R.attr.colorPrimaryContainer))
-        } else {
-            binding.root.setBackgroundColor(Color.TRANSPARENT)
-        }
-
-        val prefix = if (item.type == BillType.Income.value) "-" else "+"
-
-        binding.tvAmount.text = String.format("$prefix %.2f", item.amount) // 保留两位有效数字
-        binding.tvTime.text = DateUtils.getTime(item.time)
-        if(item.remark.isNullOrEmpty()){
-            binding.tvRemark.visibility = View.GONE
-        }else{
-            binding.tvRemark.visibility = View.VISIBLE
-            binding.tvRemark.text = item.remark
-        }
-
-        binding.tvCategory.text = item.category
-        binding.tvAccount.text = item.accountFrom
-
-
-        val colorRes = BillUtils.getColor(item.type.toInt())
-        val color = ContextCompat.getColor(context, colorRes)
-        binding.tvAmount.setTextColor(color)
-    }
 }
-*/
