94ae99926d6bf4cba51fb5f9c33167e6411717a6
ankio <ankio@ankio.net>
2024-09-20 13:05:31 +0800

    :bug: (permissions): 适配Android 14 前台服务权限
    修复 Android 14 前台服务权限问题，并添加了对 Android 11 及以上版本的 QUERY_ALL_PACKAGES 权限的处理。

--- app/src/xposed/java/net/ankio/auto/hooks/auto/AutoHooker.kt
+++ app/src/xposed/java/net/ankio/auto/hooks/auto/AutoHooker.kt
@@ -49,10 +49,20 @@ class AutoHooker : HookerManifest() {
             Manifest.permission.ACCESS_NETWORK_STATE,
             //悬浮窗权限
             Manifest.permission.SYSTEM_ALERT_WINDOW,
-          //  "android.permission.SYSTEM_OVERLAY_WINDOW",
-            // Query all packages
-            Manifest.permission.QUERY_ALL_PACKAGES,
 
             Manifest.permission.FOREGROUND_SERVICE
         )
+    init {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Android 11 (API 30)
+            permissions.add(Manifest.permission.QUERY_ALL_PACKAGES)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            // Android 14 (API 34) 需要特殊的前台服务权限
+            permissions.add(Manifest.permission.FOREGROUND_SERVICE_SPECIAL_USE)
+        }
+
+    }
 }
