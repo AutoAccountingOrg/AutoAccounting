312c0400f7ec1ac6205ed02a7073dd96ff23f7f0
ankio <ankio@ankio.net>
2024-09-27 23:33:56 +0800

    :recycle:  (ui): 移除 Github 登录功能 #597
    - 移除 Github 登录功能，改用 Pastebin 分享链接的方式进行数据请求和问题提交。
    - 更新数据请求和问题提交逻辑，使用 Pastebin API 进行数据上传。
    - 更新 UI，移除 Github 登录相关提示和按钮。

---
+++ app/src/main/java/net/ankio/auto/request/Pastebin.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2024 ankio(ankio@ankio.net)
+ * Licensed under the Apache License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-3.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package net.ankio.auto.request
+
+import android.content.Context
+import com.google.gson.JsonParser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import net.ankio.auto.storage.Logger
+import net.ankio.auto.utils.DateUtils
+
+/**
+ * 上传数据到fars.ee,并设置2个月有效期
+ */
+object Pastebin {
+    suspend fun add(data: String, context: Context):Pair<String,String> = withContext(Dispatchers.IO) {
+        val request = RequestsUtils(context)
+        request.addHeader("Accept","application/json")
+       val (code,body) = request.form(
+            "https://fars.ee/", hashMapOf(
+                "content" to data,
+                "sunset" to DateUtils.stampToDate(DateUtils.twoMonthsLater(), "yyyy-MM-dd'T'HH:mm:ss")
+            )
+        )
+
+        Logger.d("body:$body")
+
+        val json = JsonParser.parseString(body).asJsonObject
+        val url = json.get("url").asString
+        val sunset = json.get("sunset").asString
+        val timeout = DateUtils.stampToDate(DateUtils.dateToStamp(sunset,"yyyy-MM-dd'T'HH:mm:ss"))
+        Pair(url,timeout)
+    }
+}
