dca9159a1882e555e9e9ab258a3d6b53ebe9a9fe
ankio <ankio@ankio.net>
2024-11-28 23:40:34 +0800

    :bug: (wechat): 修复微信Tinker热更新导致Hook失效的问题 #785
    在加载微信应用时，通过删除Tinker补丁目录来防止热更新导致Hook失效。同时，优化了Tinker关闭逻辑，确保在调试模式下记录Tinker日志。

--- app/src/main/java/net/ankio/auto/xposed/hooks/wechat/WechatHooker.kt
+++ app/src/main/java/net/ankio/auto/xposed/hooks/wechat/WechatHooker.kt
@@ -19,7 +19,8 @@ import android.app.Application
 import de.robv.android.xposed.XposedHelpers
 import net.ankio.auto.xposed.core.api.HookerManifest
 import net.ankio.auto.xposed.core.api.PartHooker
-import net.ankio.auto.xposed.core.utils.AppUtils
+import net.ankio.auto.xposed.core.hook.Hooker
+import net.ankio.auto.xposed.core.logger.Logger
 import net.ankio.auto.xposed.hooks.wechat.hooks.ChatUserHooker
 import net.ankio.auto.xposed.hooks.wechat.hooks.DatabaseHooker
 import net.ankio.auto.xposed.hooks.wechat.hooks.PayToolsHooker
@@ -35,43 +36,33 @@ class WechatHooker : HookerManifest() {
     override val packageName: String
         get() = "com.tencent.mm"
     override val appName: String = "微信"
+
     override fun hookLoadPackage(application: Application?, classLoader: ClassLoader) {
         if (application == null) {
             return
         }
 
 
-       /* tinker日志
-       Hooker.allMethodsBefore(Hooker.loader("com.tencent.tinker.loader.shareutil.ShareTinkerLog")){
-            param->
-            val method = param.method
-            val args = param.args
-            log("ShareTinkerLog: ${method.name} ${args.joinToString(" ")}")
-        }*/
+        if (Logger.debug) {
+            Hooker.allMethodsBefore(Hooker.loader("com.tencent.tinker.loader.shareutil.ShareTinkerLog")) { param ->
+                val method = param.method
+                val args = param.args
+                log("ShareTinkerLog: ${method.name} ${args.joinToString(" ")}")
+            }
+        }
+
 
         // 腾讯tinker热更新框架在加载后会导致hook无效，最简单的办法是删掉
         // 判断目录/data/data/com.tencent.mm/tinker/下是否有patch-开头的文件夹，如果有就删除
-    /*    XposedHelpers.callStaticMethod(
-            XposedHelpers.findClass("com.tencent.tinker.loader.shareutil.ShareTinkerInternals", application.classLoader),
-            "setTinkerDisableWithSharedPreferences",
-            application
-        )*/
-       val tinkerDir = File(application.dataDir, "tinker")
+        val tinkerDir = File(application.dataDir, "tinker")
 
         if (tinkerDir.exists()) {
             log("tinkerDir: ${tinkerDir.absolutePath}")
-            tinkerDir.listFiles()?.forEach {
-                if (it.isDirectory && it.name.startsWith("patch-")) {
-                    log("find tinker patch dir: ${it.absolutePath}, delete it")
-                    it.deleteRecursively()
-                }
-           }
-          closeTinker(classLoader)
+            closeTinker(classLoader)
         }
 
 
     }
-
 
 
     override var minVersion: Int
@@ -86,21 +77,7 @@ class WechatHooker : HookerManifest() {
         ChatUserHooker(),
         PayToolsHooker()
     )
-    override fun beforeAdapter(application: Application,file:String){
-   runCatching {
 
-           //TODO 删除tinker会导致微信冷启动变慢，主要原因是删除后微信加载tinker补丁失败，会启动另一套启动流程，甚至还会频繁触发微信闪退错误页面。
-           //TODO 这里需要重新优化Xposed hook，应该hook微信加载tinker之后的application，而不是简单粗暴的删除tinker补丁。
-          if (file.contains("tinker")) {
-              log("disable tinker patch dir: $file")
-           //    File(file).delete()
-              closeTinker(application.classLoader)
-               AppUtils.restart()
-           }
-       }.onFailure{
-           logE(it)
-       }
-    }
     override var rules: MutableList<Clazz>
         get() = mutableListOf(
             Clazz(
@@ -217,17 +194,30 @@ class WechatHooker : HookerManifest() {
     )
 
 
-    private fun closeTinker(classLoader: ClassLoader){
-        val tinkerManager = runCatching { XposedHelpers.findClass("com.tencent.tinker.lib.tinker.TinkerApplicationHelper",classLoader) }.getOrNull()?:return
-        val applicationLike = XposedHelpers.callStaticMethod(tinkerManager,"getTinkerApplicationLike") as Class<*>
-        val shareTinkerInternals = XposedHelpers.findClass("com.tencent.tinker.loader.shareutil.ShareTinkerInternals", classLoader)
+    private fun closeTinker(classLoader: ClassLoader) {
+        val tinkerManager = runCatching {
+            XposedHelpers.findClass(
+                "com.tencent.tinker.lib.tinker.TinkerApplicationHelper",
+                classLoader
+            )
+        }.getOrNull() ?: return
+        val applicationLike =
+            XposedHelpers.callStaticMethod(tinkerManager, "getTinkerApplicationLike") as Class<*>
+        val shareTinkerInternals = XposedHelpers.findClass(
+            "com.tencent.tinker.loader.shareutil.ShareTinkerInternals",
+            classLoader
+        )
         //关闭其他进程
-        val application = XposedHelpers.callMethod(applicationLike,"getApplication")
+        val application = XposedHelpers.callMethod(applicationLike, "getApplication")
         XposedHelpers.callStaticMethod(shareTinkerInternals, "killAllOtherProcess", application)
         //清除补丁
         XposedHelpers.callStaticMethod(shareTinkerInternals, "cleanPatch", applicationLike)
         //关闭tinker
-        XposedHelpers.callStaticMethod(shareTinkerInternals, "setTinkerDisableWithSharedPreferences", application)
+        XposedHelpers.callStaticMethod(
+            shareTinkerInternals,
+            "setTinkerDisableWithSharedPreferences",
+            application
+        )
     }
 
 
