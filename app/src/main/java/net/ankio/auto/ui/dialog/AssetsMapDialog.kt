cada826512de884065cc97eea5e21598a0afb749
ankio <ankio@ankio.net>
2024-12-14 14:13:21 +0800

    :recycle: (ui): 重构 AssetsMapDialog 代码结构
    将初始化视图和事件监听器逻辑分离，提升代码可读性和维护性。

--- app/src/main/java/net/ankio/auto/ui/dialog/AssetsMapDialog.kt
+++ app/src/main/java/net/ankio/auto/ui/dialog/AssetsMapDialog.kt
@@ -22,6 +22,7 @@ import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 import net.ankio.auto.R
 import net.ankio.auto.databinding.DialogMapBinding
+import net.ankio.auto.storage.Logger
 import net.ankio.auto.ui.api.BaseSheetDialog
 import net.ankio.auto.ui.utils.ResourceUtils
 import net.ankio.auto.ui.utils.ToastUtils
@@ -30,64 +31,81 @@ import org.ezbook.server.db.model.AssetsMapModel
 class AssetsMapDialog(
     private val context: Context,
     private val assetsMapModel: AssetsMapModel = AssetsMapModel(),
-    val onClose: (AssetsMapModel) -> Unit,
+    private val onClose: (AssetsMapModel) -> Unit,
 ) : BaseSheetDialog(context) {
     private lateinit var binding: DialogMapBinding
 
     override fun onCreateView(inflater: LayoutInflater): View {
         binding = DialogMapBinding.inflate(inflater)
-        cardView = binding.cardView
-        cardViewInner = binding.cardViewInner
-
-        if (assetsMapModel.id != 0L) {
-            setBindingData()
-        } else {
-            binding.target.setText(context.getString(R.string.map_no_target))
-        }
-        bindingEvents()
         return binding.root
     }
 
-    private fun setBindingData() {
-        binding.raw.setText(assetsMapModel.name)
-        binding.target.setText(assetsMapModel.mapName)
-        binding.regex.isChecked = assetsMapModel.regex
-        lifecycleScope.launch {
-            ResourceUtils.getAssetDrawableFromName(assetsMapModel.mapName).let {
-                binding.target.setIcon(it)
-            }
+    override fun onViewCreated(view: View) {
+        super.onViewCreated(view)
+        initializeView()
+        setupEventListeners()
+    }
+
+    private fun initializeView() {
+        if (assetsMapModel.id != 0L) {
+            setBindingData()
         }
     }
 
-    private fun bindingEvents() {
-        binding.buttonCancel.setOnClickListener { dismiss() }
+    private fun setBindingData() = with(binding) {
+        raw.setText(assetsMapModel.name)
+        if(assetsMapModel.mapName.isEmpty()) {
+            target.setText(context.getString(R.string.map_target))
+        } else {
+            target.setText(assetsMapModel.mapName)
+        }
+        regex.isChecked = assetsMapModel.regex
+        
+        lifecycleScope.launch {
+            ResourceUtils.getAssetDrawableFromName(assetsMapModel.mapName)
+                .let { target.setIcon(it) }
+        }
+    }
 
-        binding.buttonSure.setOnClickListener {
-            assetsMapModel.name = binding.raw.text.toString()
-            assetsMapModel.mapName = binding.target.getText()
-            assetsMapModel.regex = binding.regex.isChecked
+    private fun setupEventListeners() = with(binding) {
+        buttonCancel.setOnClickListener { dismiss() }
+        buttonSure.setOnClickListener { handleSaveAction() }
+        target.setOnClickListener { showAssetSelector() }
+    }
 
-            if (assetsMapModel.name.isEmpty() || assetsMapModel.mapName == context.getString(R.string.map_no_target)) {
-                ToastUtils.error(context.getString(R.string.map_empty))
-                return@setOnClickListener
+    private fun handleSaveAction() = with(binding) {
+        val name = raw.text.toString()
+        val mapName = target.getText()
+        
+        if (name.isEmpty() || mapName == context.getString(R.string.map_target)) {
+            ToastUtils.error(context.getString(R.string.map_empty))
+        } else{
+            assetsMapModel.apply {
+                this.name = name
+                this.mapName = mapName
+                this.regex = binding.regex.isChecked
             }
-
             lifecycleScope.launch {
                 onClose(assetsMapModel)
                 dismiss()
             }
         }
-        binding.target.setOnClickListener {
-            AssetsSelectorDialog(context) {
-                assetsMapModel.mapName = it.name
-                binding.target.setText(it.name)
 
-                lifecycleScope.launch {
-                    ResourceUtils.getAssetDrawable(it.icon).let { icon ->
-                        binding.target.setIcon(icon)
-                    }
-                }
-            }.show(cancel = true)
+    }
+
+    private fun showAssetSelector() {
+       val dialog = AssetsSelectorDialog(context) { asset ->
+            assetsMapModel.mapName = asset.name
+            binding.target.setText(asset.name)
+
+            lifecycleScope.launch {
+                ResourceUtils.getAssetDrawable(asset.icon)
+                    .let { binding.target.setIcon(it) }
+            }
         }
+        if (lifecycleOwner != null) {
+            dialog.bindToLifecycle(lifecycleOwner!!)
+        }
+        dialog.show()
     }
 }
