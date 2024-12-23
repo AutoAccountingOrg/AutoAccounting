abd525a9db23344f3b7ac240095368436a43f930
ankio <ankio@ankio.net>
2024-12-23 16:02:34 +0800

    :recycle: (ui): 重构 SettingItemView 初始化逻辑
    在 SettingItemView 中，使用 `try-finally` 结构重构自定义属性的获取和回收逻辑，确保资源正确释放。

--- app/src/main/java/net/ankio/auto/ui/componets/SettingItemView.kt
+++ app/src/main/java/net/ankio/auto/ui/componets/SettingItemView.kt
@@ -14,25 +14,30 @@ class SettingItemView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
+
     private var binding: ViewSettingItemBinding =
         ViewSettingItemBinding.inflate(LayoutInflater.from(context), this, true)
 
     init {
 
         // 获取自定义属性
-        context.obtainStyledAttributes(attrs, R.styleable.SettingItemView).use { typedArray ->
-            // 设置图标
-            val iconRes = typedArray.getResourceId(R.styleable.SettingItemView_settingIcon, 0)
-            if (iconRes != 0) {
-                binding.settingIcon.setImageResource(iconRes)
-            }
-            
-            // 设置标题
-            binding.settingTitle.text = typedArray.getString(R.styleable.SettingItemView_settingTitle)
-            
-            // 设置描述
-            binding.settingDesc.text = typedArray.getString(R.styleable.SettingItemView_settingDesc)
+        context.obtainStyledAttributes(attrs, R.styleable.SettingItemView).apply {
 
+            try { // 设置图标
+                val iconRes = getResourceId(R.styleable.SettingItemView_settingIcon, 0)
+                if (iconRes != 0) {
+                    binding.settingIcon.setImageResource(iconRes)
+                }
+
+                // 设置标题
+                binding.settingTitle.text = getString(R.styleable.SettingItemView_settingTitle)
+
+                // 设置描述
+                binding.settingDesc.text = getString(R.styleable.SettingItemView_settingDesc)
+
+            }finally {
+                recycle()
+            }
         }
     }
 
