681e6b25fb54cd61eb10fa73b745dc0ad00aae10
ankio <ankio@ankio.net>
2025-01-13 15:49:46 +0800

    :sparkles: (qianji): 优化资产管理的账户名称处理逻辑
    在启用资产管理时，根据账单类型和资产是否存在动态添加 `accountname` 和 `accountname2` 参数。新增对 `IGNORE_ASSET` 配置的支持，允许忽略资产检查。

--- app/src/main/java/net/ankio/auto/xposed/hooks/qianji/tools/QianJiUri.kt
+++ app/src/main/java/net/ankio/auto/xposed/hooks/qianji/tools/QianJiUri.kt
@@ -18,12 +18,18 @@ package net.ankio.auto.xposed.hooks.qianji.tools
 import android.net.Uri
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import net.ankio.auto.xposed.core.utils.DataUtils
+import net.ankio.auto.xposed.hooks.qianji.impl.AssetPreviewPresenterImpl
 import net.ankio.auto.xposed.hooks.qianji.sync.AutoConfig
 import org.ezbook.server.db.model.BillInfoModel
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import net.ankio.auto.xposed.hooks.qianji.tools.formatTime
+import org.ezbook.server.constant.BillType
+import org.ezbook.server.constant.DefaultData
+import org.ezbook.server.constant.Setting
+import org.ezbook.server.db.model.SettingModel
 
 object QianJiUri {
     suspend fun toQianJi(billModel: BillInfoModel): Uri = withContext(Dispatchers.IO) {
@@ -45,15 +51,49 @@ object QianJiUri {
         }
 
         if (AutoConfig.assetManagement) {
-            uri.append("&accountname=${Uri.encode(billModel.accountNameFrom)}")
-            uri.append("&accountname2=${Uri.encode(billModel.accountNameTo)}")
-        }
+            val ignore = DataUtils.configBoolean(Setting.IGNORE_ASSET, DefaultData.IGNORE_ASSET)
+            if (listOf(
+                    BillType.Expend,//支出
 
+                    BillType.ExpendReimbursement,//支出（记作报销）
+                    BillType.ExpendLending,//支出（借出）
+                    BillType.ExpendRepayment,//支出（还款）
+
+                    BillType.Income,//收入
+
+                    BillType.IncomeReimbursement,//收入（报销）
+                    BillType.IncomeRefund, //收入（退款）
+
+                    BillType.Transfer//转账
+                ).contains(billModel.type)
+            ) {
+                val assetFromExists =
+                    AssetPreviewPresenterImpl.getAssetByName(billModel.accountNameFrom) != null
+                if (assetFromExists || ignore) {
+                    uri.append("&accountname=${Uri.encode(billModel.accountNameFrom)}")
+                }
+
+            }
+            if (listOf(
+                    BillType.IncomeLending,//收入（借入）
+                    BillType.IncomeRepayment,//收入（收款）
+                    BillType.Transfer//转账
+                ).contains(billModel.type)
+            ) {
+
+                // 检查是否存在对应的资产，如果不存在但不忽略，则不添加 accountname2
+                val assetExists =
+                    AssetPreviewPresenterImpl.getAssetByName(billModel.accountNameTo) != null
+                if (assetExists || ignore) {
+                    uri.append("&accountname2=${Uri.encode(billModel.accountNameTo)}")
+                }
+            }
+
+        }
 
         if (AutoConfig.fee) {
             uri.append("&fee=${billModel.fee}")
         }
-
 
         if (AutoConfig.multiCurrency) {
             uri.append("&currency=${billModel.currency}")
