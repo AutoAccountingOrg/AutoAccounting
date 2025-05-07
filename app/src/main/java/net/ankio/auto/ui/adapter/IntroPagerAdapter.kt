25a5b3b472760dbb08739b62184c384f7728db35
ankio <ankio@ankio.net>
2025-05-07 12:36:19 +0800

    :sparkles: (ui): 添加第二个介绍页面和相关资源
    添加了新的 IntroPage2Fragment 用于应用的介绍流程，并更新了 IntroPagerAdapter 来包含这个新页面。此外，加入了新的 vector 图标和布局文件以增强 UI 的互动性和视觉表现。这些改动是为了改善用户的引导体验，确保应用更加直观和吸引用户。

--- app/src/main/java/net/ankio/auto/ui/adapter/IntroPagerAdapter.kt
+++ app/src/main/java/net/ankio/auto/ui/adapter/IntroPagerAdapter.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import net.ankio.auto.ui.fragment.intro.IntroPage1Fragment
+import net.ankio.auto.ui.fragment.intro.IntroPage2Fragment
 
 class IntroPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
     override fun getItemCount() = 3 // 页数
@@ -11,7 +12,7 @@ class IntroPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activ
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> IntroPage1Fragment()
-            1 -> IntroPage1Fragment()
+            1 -> IntroPage2Fragment()
             2 -> IntroPage1Fragment()
             else -> throw IllegalArgumentException("Invalid page")
         }
---
+++ app/src/main/java/net/ankio/auto/ui/fragment/intro/IntroPage2Fragment.kt
@@ -0,0 +1,20 @@
+package net.ankio.auto.ui.fragment.intro
+
+import android.os.Bundle
+import android.view.View
+import net.ankio.auto.R
+import net.ankio.auto.databinding.FragmentIntroPage2Binding
+import net.ankio.auto.ui.api.BaseFragment
+
+class IntroPage2Fragment : BaseFragment<FragmentIntroPage2Binding>() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
