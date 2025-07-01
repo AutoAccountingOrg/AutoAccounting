608c8e8e4a4a482c20d8f36982703a260b0a7853
ankio <ankio@ankio.net>
2025-07-01 10:51:54 +0800

    :sparkles: (ui): 实现规则数据上传和问题反馈功能
    - 新增数据上传功能，支持通过Pastebin分享数据
    - 添加规则适配请求和规则错误反馈两种提交方式
    - 根据网络情况自动选择Github或云端API提交问题
    - 完善长按删除数据功能
    - 优化AI生成规则的提示信息
    - 移除未使用的配置项导入

--- app/src/main/java/net/ankio/auto/ui/adapter/AppDataAdapter.kt
+++ app/src/main/java/net/ankio/auto/ui/adapter/AppDataAdapter.kt
@@ -16,28 +16,34 @@
 package net.ankio.auto.ui.adapter
 
 import android.content.Intent
+import android.net.Uri
 import android.view.View
+import androidx.core.net.toUri
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.elevation.SurfaceColors
 import com.google.gson.Gson
+import com.google.gson.JsonObject
 import kotlinx.coroutines.launch
 import net.ankio.auto.App
 import net.ankio.auto.R
 import net.ankio.auto.databinding.AdapterDataBinding
+import net.ankio.auto.http.Pastebin
 import net.ankio.auto.http.api.JsAPI
+import net.ankio.auto.http.license.RuleAPI
 import net.ankio.auto.intent.IntentType
 import net.ankio.auto.service.CoreService
-import net.ankio.auto.storage.ConfigUtils
 import net.ankio.auto.storage.Logger
 import net.ankio.auto.ui.api.BaseActivity
 import net.ankio.auto.ui.api.BaseAdapter
 import net.ankio.auto.ui.api.BaseViewHolder
 import net.ankio.auto.ui.dialog.BottomSheetDialogBuilder
+import net.ankio.auto.ui.dialog.DataEditorDialog
+import net.ankio.auto.ui.dialog.EditorDialogBuilder
 import net.ankio.auto.ui.utils.LoadingUtils
 import net.ankio.auto.ui.utils.ToastUtils
+import net.ankio.auto.utils.CustomTabsHelper
 import net.ankio.auto.utils.DateUtils
-import org.ezbook.server.constant.DefaultData
-import org.ezbook.server.constant.Setting
+import net.ankio.auto.utils.PrefManager
 import org.ezbook.server.db.model.AppDataModel
 
 class AppDataAdapter(
@@ -58,14 +64,54 @@ class AppDataAdapter(
             onContentClick(it, holder.item!!)
         }
 
-        // TODO 上传
+
 
         binding.uploadData.setOnClickListener {
+            val item = holder.item!!
+            // AI生成的不支持上传
+            if (item.rule.endsWith("生成")) {
+                ToastUtils.error(R.string.ai_not_support)
+                return@setOnClickListener
+            }
+            DataEditorDialog(activity, item.data) { result ->
 
+                activity.lifecycleScope.launch {
+                    val loading = LoadingUtils(activity)
+                    loading.show(R.string.upload_waiting)
+                    if (!item.match) {
+                        requestRuleHelp(item, result)
+                    } else {
+                        EditorDialogBuilder(activity)
+                            .setTitleInt(R.string.rule_issue)
+                            .setMessage("")
+                            .setEditorPositiveButton(R.string.btn_confirm) {
+                                activity.lifecycleScope.launch {
+                                    requestRuleBug(item, result, it)
+                                }
+                            }.setPositiveButton(R.string.btn_cancel) { _, _ ->
+
+                            }.show()
+
+                    }
+                    loading.close()
+                }
+
+            }
         }
 
         binding.root.setOnLongClickListener {
-
+            val item = holder.item!!
+            BottomSheetDialogBuilder(activity)
+                .setTitle(activity.getString(R.string.delete_title))
+                .setMessage(activity.getString(R.string.delete_data_message))
+                .setPositiveButton(activity.getString(R.string.btn_confirm)) { _, _ ->
+                    removeItem(item)
+                    activity.lifecycleScope.launch {
+                        AppDataModel.delete(item.id)
+                    }
+                }
+                .setNegativeButton(activity.getString(R.string.btn_cancel)) { _, _ -> }
+                .show()
             true
         }
 
@@ -95,8 +141,7 @@ class AppDataAdapter(
         val loadingUtils = LoadingUtils(activity)
         loadingUtils.show(
             activity.getString(
-                R.string.ai_loading,
-                ConfigUtils.getString(Setting.AI_MODEL)
+                R.string.ai_loading
             )
         )
 
@@ -162,5 +207,93 @@ class AppDataAdapter(
 
     override fun areContentsSame(oldItem: AppDataModel, newItem: AppDataModel): Boolean {
         return oldItem == newItem
+    }
+
+
+    suspend fun requestRuleHelp(item: AppDataModel, result: String) {
+        val type = item.type.name
+        val title = "[Adaptation Request][$type]${item.app}"
+        runCatching {
+            val (url, timeout) = Pastebin.add(result)
+            val body = """
+<!------ 
+ 1. 请不要手动复制数据，下面的链接中已包含数据；
+ 2. 您可以新增信息，但是不要删除本页任何内容；
+ 3. 一般情况下，您直接划到底部点击submit即可。
+ ------>                        
+## 数据链接                        
+[数据过期时间：${timeout}](${url})
+## 其他信息
+<!------ 
+ 1. 您可以在下面添加说明信息。
+ ------>  
+
+                """.trimIndent()
+
+            //上传Github Or 使用API
+            if (PrefManager.githubConnectivity) {
+                submitGithub(title, body)
+            } else {
+                submitCloud(title, body)
+            }
+
+        }.onFailure {
+            Logger.e(it.message ?: "", it)
+            ToastUtils.error(it.message ?: "Error in post issue")
+        }
+    }
+
+    suspend fun requestRuleBug(item: AppDataModel, result: String, desc: String) {
+        val type = item.type.name
+        val title = "[Bug][Rule][$type]${item.app}"
+        runCatching {
+            val (url, timeout) = Pastebin.add(result)
+            val body = """
+<!------ 
+ 1. 请不要手动复制数据，下面的链接中已包含数据；
+ 2. 该功能是反馈规则识别错误的，请勿写其他无关内容；
+ ------>  
+## 规则
+${item.rule}
+## 数据
+[数据过期时间：${timeout}](${url})
+## 说明
+${desc}
+
+                         
+                                            """.trimIndent()
+
+            //上传Github Or 使用API
+            if (PrefManager.githubConnectivity) {
+                submitGithub(title, body)
+            } else {
+                submitCloud(title, body)
+            }
+
+        }.onFailure {
+            Logger.e(it.message ?: "", it)
+            ToastUtils.error(it.message ?: "Error in post issue")
+        }
+    }
+
+    // Github可访问的话，直接通过Github提交
+
+    fun submitGithub(title: String, body: String) {
+        val uri = "https://github.com/AutoAccountingOrg/AutoRuleSubmit/issues"
+        CustomTabsHelper.launchUrl(
+            activity,
+            "$uri/new?title=${Uri.encode(title)}&body=${Uri.encode(body)}".toUri(),
+        )
+    }
+
+
+    // Github不可访问的话，直接通过云端代理提交
+
+    suspend fun submitCloud(title: String, body: String) {
+        val result = RuleAPI.submit(title, body)
+        val data = Gson().fromJson(result, JsonObject::class.java)
+        if (data.get("code").asInt != 200) {
+            throw RuntimeException(data.get("msg").asString)
+        }
     }
 }
