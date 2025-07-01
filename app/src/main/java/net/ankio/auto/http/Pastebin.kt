63843811f257ef8a9477678d25adf7931e903be2
ankio <ankio@ankio.net>
2025-07-01 10:50:09 +0800

    :truck: (http): 移动并重构Pastebin类到http包
    将Pastebin工具类从request包移动到http包，并做以下优化：
    - 移除不必要的Context参数依赖
    - 更新版权年份至2025
    - 清理多余导入和日志代码
    - 优化类注释说明

--- app/src/main/java/net/ankio/auto/http/Pastebin.kt
+++ app/src/main/java/net/ankio/auto/http/Pastebin.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 ankio(ankio@ankio.net)
+ * Copyright (C) 2025 ankio(ankio@ankio.net)
  * Licensed under the Apache License, Version 3.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,9 +13,8 @@
  *   limitations under the License.
  */
 
-package net.ankio.auto.request
+package net.ankio.auto.http
 
-import android.content.Context
 import com.google.gson.JsonParser
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -23,11 +22,11 @@ import net.ankio.auto.storage.Logger
 import net.ankio.auto.utils.DateUtils
 
 /**
- * 上传数据到fars.ee,并设置2个月有效期
+ * 上传数据到Pastebin,并设置2个月有效期
  */
 object Pastebin {
-    suspend fun add(data: String, context: Context):Pair<String,String> = withContext(Dispatchers.IO) {
-        val request = RequestsUtils(context)
+    suspend fun add(data: String): Pair<String, String> = withContext(Dispatchers.IO) {
+        val request = RequestsUtils()
         request.addHeader("Accept","application/json")
        val (code,body) = request.form(
            "https://bin.ankio.net/", hashMapOf(
@@ -35,8 +34,6 @@ object Pastebin {
                "ttl" to (60 * 24 * 60).toString()
             )
         )
-
-        Logger.d("body:$body")
 
         val json = JsonParser.parseString(body).asJsonObject
         val url = json.get("data").asString
