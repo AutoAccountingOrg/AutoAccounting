54dd306c162990de3589dceefd938b23caab05f4
ankio <ankio@ankio.net>
2026-02-10 19:46:19 +0800

    :arrow_up: (deps): 升级 Gradle、编译 SDK 及依赖库版本 升级 Gradle 包装器至 8.11.1，AGP 至 8.9.1，Kotlin 至 2.3.0， 并同步更新 compileSdk 版本至 36。引入 Luban 图片压缩库以优化资源处理。

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
     }
 }
 
